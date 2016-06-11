320b78e9d52e504bb80b1e8be711b91313c3b0bb
sahransirang <sahranakhss10@gmail.com>
2016-06-11 14:19:21 +0500

    Update mail1.php

--- mail1.php
+++ mail1.php
@@ -1,15 +1,12 @@
 <?php
-
 $to = $_POST['to'];
 $from = $_POST['from'];
-
-$header = "from: $from";
+$header = "from: $from ";
 $subject = $_POST['subject'];
 $body = $_POST['body'];
 
 mail($to,$subject,$body,$header);
 
 echo "Email sent";
-
 
 ?>
